2960c9594834a79f91bc176f45eb767fb7eebf61
RyChoszet <pmmail100305@gmail.com>
2026-01-29 21:17:24 +0100

    Update driver_dht11_interface_template.c

--- Publisher+Subscriber/driver_dht11_interface_template.c
+++ Publisher+Subscriber/driver_dht11_interface_template.c
@@ -1,136 +1,113 @@
 /**
- * Copyright (c) 2015 - present LibDriver All rights reserved
- * 
- * The MIT License (MIT)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE. 
- *
- * @file      driver_dht11_interface_template.c
- * @brief     driver dht11 interface template source file
- * @version   2.0.0
- * @author    Shifeng Li
- * @date      2021-03-12
- *
- * <h3>history</h3>
- * <table>
- * <tr><th>Date        <th>Version  <th>Author      <th>Description
- * <tr><td>2021/03/12  <td>2.0      <td>Shifeng Li  <td>format the code
- * <tr><td>2020/11/19  <td>1.0      <td>Shifeng Li  <td>first upload
- * </table>
+ * Interfejs dla biblioteki libdriver DHT11 dla Raspberry Pi z GPIO5
  */
-
 #include "driver_dht11_interface.h"
+#include "dht11.h"  // Dodano do użycia DHT11_PIN
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * @brief  interface bus init
+ * @brief  Inicjalizacja interfejsu GPIO dla GPIO5
  * @return status code
  *         - 0 success
  *         - 1 bus init failed
- * @note   none
  */
-uint8_t dht11_interface_init(void)
-{
+uint8_t dht11_interface_init(void) {
+    printf("dht11_interface: Inicjalizacja GPIO%d...\n", DHT11_PIN);
+    
+    if (wiringPiSetupGpio() == -1) {
+        printf("dht11_interface: wiringPiSetupGpio failed\n");
+        return 1;
+    }
+    
+    // Ustaw pin DHT11 (GPIO5) jako wyjście na początku
+    pinMode(DHT11_PIN, OUTPUT);
+    digitalWrite(DHT11_PIN, HIGH);
+    
+    // Daj czas na stabilizację
+    delay(1000);
+    
+    printf("dht11_interface: GPIO%d zainicjalizowany\n", DHT11_PIN);
     return 0;
 }
 
 /**
- * @brief  interface bus deinit
+ * @brief  Deinicjalizacja interfejsu GPIO
  * @return status code
  *         - 0 success
  *         - 1 bus deinit failed
- * @note   none
  */
-uint8_t dht11_interface_deinit(void)
-{
+uint8_t dht11_interface_deinit(void) {
+    pinMode(DHT11_PIN, INPUT);
+    printf("dht11_interface: GPIO%d zdeinicjalizowany\n", DHT11_PIN);
     return 0;
 }
 
 /**
- * @brief      interface bus read
+ * @brief      Odczyt z GPIO5
  * @param[out] *value pointer to a value buffer
  * @return     status code
  *             - 0 success
  *             - 1 read failed
- * @note       none
  */
-uint8_t dht11_interface_read(uint8_t *value)
-{
+uint8_t dht11_interface_read(uint8_t *value) {
+    *value = digitalRead(DHT11_PIN);
     return 0;
 }
 
 /**
- * @brief     interface bus write
+ * @brief     Zapis do GPIO5
  * @param[in] value written value
  * @return    status code
  *            - 0 success
  *            - 1 write failed
- * @note      none
  */
-uint8_t dht11_interface_write(uint8_t value)
-{
+uint8_t dht11_interface_write(uint8_t value) {
+    digitalWrite(DHT11_PIN, value);
     return 0;
 }
 
 /**
- * @brief     interface delay ms
- * @param[in] ms time
- * @note      none
+ * @brief     Opóźnienie w milisekundach
+ * @param[in] ms czas w milisekundach
  */
-void dht11_interface_delay_ms(uint32_t ms)
-{
-    
+void dht11_interface_delay_ms(uint32_t ms) {
+    delay(ms);
 }
 
 /**
- * @brief     interface delay us
- * @param[in] us time
- * @note      none
+ * @brief     Opóźnienie w mikrosekundach
+ * @param[in] us czas w mikrosekundach
  */
-void dht11_interface_delay_us(uint32_t us)
-{
-    
+void dht11_interface_delay_us(uint32_t us) {
+    delayMicroseconds(us);
 }
 
 /**
- * @brief interface enable the interrupt
- * @note  none
+ * @brief Włącz przerwania (nie używane w tym projekcie)
  */
-void dht11_interface_enable_irq(void)
-{
-    
+void dht11_interface_enable_irq(void) {
+    // W tym projekcie nie używamy przerwań
 }
 
 /**
- * @brief interface disable the interrupt
- * @note  none
+ * @brief Wyłącz przerwania (nie używane w tym projekcie)
  */
-void dht11_interface_disable_irq(void)
-{
-    
+void dht11_interface_disable_irq(void) {
+    // W tym projekcie nie używamy przerwań
 }
 
 /**
- * @brief     interface print format data
- * @param[in] fmt format data
- * @note      none
+ * @brief     Debug print
+ * @param[in] fmt format string
  */
-void dht11_interface_debug_print(const char *const fmt, ...)
-{
-    
+void dht11_interface_debug_print(const char *const fmt, ...) {
+#ifdef DEBUG_DHT11
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+#endif
 }
