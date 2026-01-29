b742e8a4be0da676db2b7709dce48fe7c3ba8bb7
RyChoszet <pmmail100305@gmail.com>
2026-01-29 19:30:44 +0100

    possible resolve for not stopping

--- Publisher+Subscriber/dht11.c
+++ Publisher+Subscriber/dht11.c
@@ -2,13 +2,14 @@
 #include <wiringPi.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <sys/time.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Inicjalizacja wiringPi i czujnika DHT11
 int DHT11_Init() {
     // Inicjalizacja wiringPi
-    if (wiringPiSetupPinType(WPI_PIN_WPI) == -1) {
-        perror("wiringPiSetup");
+    if (wiringPiSetupGpio() == -1) {
+        fprintf(stderr, "Błąd: Nie można zainicjalizować wiringPi\n");
         return -1;
     }
     
@@ -16,76 +17,72 @@ int DHT11_Init() {
     pinMode(DHT11_PIN, OUTPUT);
     digitalWrite(DHT11_PIN, HIGH);
     
+    // Opóźnienie inicjalizacji
+    delay(1000);
+    
     return 0;
 }
 
-// Odczyt danych z czujnika DHT11
-int DHT11_ReadSensor(float *temperature, float *humidity){
-	uint64_t counter = 0;
-	uint8_t j, i;
-	int dht11_dat[41];
-	unsigned long time_in_micros;
-	struct timeval return_of_sig;
-	pinMode(DHT11_PIN, OUTPUT);
-	digitalWrite(DHT11_PIN, LOW);
-	delay(2);
-	pinMode(DHT11_PIN, INPUT);
-	for(i = 1; i < 2000; i++){
-		if(digitalRead(DHT11_PIN) == 0){
-			break;
-		}
-	}
-	for(j=0; j < 41; j++){
-		for(i = 1; i < 2000; i++){
-			if(digitalRead(DHT11_PIN) == 1){
-				break;
-			}
-		}
-		
-		gettimeofday(&return_of_sig, NULL);
-		time_in_micros = 1000000 * return_of_sig.tv_sec + return_of_sig.tv_usec;
-		
-		for(i = 1; i < 2000; i++){
-			if(digitalRead(DHT11_PIN) == 0){
-				break;
-			}
-		}
-		
-		gettimeofday(&return_of_sig, NULL);
-		dht11_dat[j] = ((1000000 * return_of_sig.tv_sec + return_of_sig.tv_usec) - time_in_micros) > 50;
-	}
-	
-	int byte1 = getByte(1, dht11_dat);
-	int byte2 = getByte(2, dht11_dat);
-	int byte3 = getByte(3, dht11_dat);
-	int byte4 = getByte(4, dht11_dat);
-	int byte5 = getByte(5, dht11_dat);
-	
-	pinMode(DHT11_PIN, OUTPUT);
-	digitalWrite(DHT11_PIN, HIGH);
-	
-	if(byte5 == ((byte1+byte2+byte3+byte4)&0xFF)){
-		float humidity_temp = (float)(byte1 << 8 | byte2)/10.0;
-		int neg = byte3 & 0x80;
-		byte3 = byte3 & 0x7F;
-		float temperature_temp = (float)(byte3 << 8 | byte4) / 10.0;
-		if(neg > 0){
-			temperature_temp = -temperature_temp;
-		}
-		*humidity = humidity_temp;
-		*temperature = temperature_temp;
-		return 0;
-	}
-	return -1;
-}
-
-int getByte(int b, int buf[]){
-	int i;
-	int result = 0;
-	b = (b - 1)*8 + 1;
-	for(i = b; i <= b+7; i++){
-		result = result << 1;
-		result = result | buf[i];
-	}
-	return result;
+// Odczyt danych z czujnika DHT11 (poprawiona wersja)
+int DHT11_ReadSensor(float *temperature, float *humidity) {
+    uint8_t data[5] = {0, 0, 0, 0, 0};
+    uint8_t laststate = HIGH;
+    uint8_t counter = 0;
+    uint8_t j = 0, i;
+    
+    // Czekaj na gotowość czujnika
+    digitalWrite(DHT11_PIN, HIGH);
+    delay(250);
+    
+    // Rozpocznij odczyt
+    pinMode(DHT11_PIN, OUTPUT);
+    digitalWrite(DHT11_PIN, LOW);
+    delay(18);
+    
+    // Przygotuj pin do odczytu
+    digitalWrite(DHT11_PIN, HIGH);
+    delayMicroseconds(40);
+    pinMode(DHT11_PIN, INPUT);
+    
+    // Odczytaj 40 bitów danych
+    for (i = 0; i < 85; i++) {
+        counter = 0;
+        while (digitalRead(DHT11_PIN) == laststate) {
+            counter++;
+            delayMicroseconds(1);
+            if (counter == 255) {
+                break;
+            }
+        }
+        laststate = digitalRead(DHT11_PIN);
+        
+        if (counter == 255) {
+            break;
+        }
+        
+        // Ignoruj pierwsze 3 przejścia
+        if ((i >= 4) && (i % 2 == 0)) {
+            // Zbierz każdy bit
+            data[j / 8] <<= 1;
+            if (counter > 16) {
+                data[j / 8] |= 1;
+            }
+            j++;
+        }
+    }
+    
+    // Sprawdź sumę kontrolną i zwróc dane
+    if ((j >= 40) && (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF))) {
+        *humidity = (float)data[0] + (float)data[1] / 10.0f;
+        *temperature = (float)data[2] + (float)data[3] / 10.0f;
+        
+        // Sprawdź poprawność zakresów
+        if (*humidity > 100.0f || *temperature > 80.0f || *temperature < 0.0f) {
+            return -1; // Nieprawidłowe wartości
+        }
+        
+        return 0;
+    } else {
+        return -1; // Błąd sumy kontrolnej
+    }
 }
