2087dcd4a519efbbfaaa854d92e81049840bf3fa
RyChoszet <pmmail100305@gmail.com>
2026-01-30 16:43:13 +0100

    Update dht11.h

--- Publisher+Subscriber/dht11.h
+++ Publisher+Subscriber/dht11.h
@@ -1,9 +1,9 @@
 #ifndef DHT11_H
 #define DHT11_H
 
-// Definicje dla DHT11 - ZMIENIONE NA GPIO5 (fizyczny pin 29)
-#define DHT11_PIN 5  // GPIO5 (fizyczny pin 29 na Raspberry Pi)
-#define DHT11_WAIT_TIME 2  // 2 sekundy między odczytami
+// Definicje dla DHT11, podłączenie na GPIO5
+#define DHT11_PIN 5  // wybranie GPIO5 (fizyczny pin 29 na Raspberry Pi)
+#define DHT11_WAIT_TIME 2  // Czas przerwy między odczytami, ok. 2 sekundy
 
 // Funkcje do obsługi czujnika DHT11
 int DHT11_Init();
